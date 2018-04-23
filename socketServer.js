d4315bfad3aa4aa80542abd1ae803c78a4943b0a
davidhiguet <38644243+davidhiguet@users.noreply.github.com>
2018-04-23 09:34:53 +0200

    Update socketServer.js

--- socketServer.js
+++ socketServer.js
@@ -33,94 +33,94 @@ var save = true;
    };
  }());
 
-var uniqueId = function () {
-    const randomLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
-    return randomLetter + Date.now();
-};
+var stagingPoint = {
 
-var play = function (socket, room) {
-   console.log(socket.id)
-     io.to(room).emit('letSGo', socket.id);
- };
+  uniqueId : function () {
+      const randomLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+      return randomLetter + Date.now();
+  },
 
-var createRoom = function (){
-   let room = uniqueId();
-   availableRooms.push(room);
-   games[room] = newGame(room);
-   return room;
- };
+  play : function (socket, room) {
+    console.log(socket.id)
+      io.to(room).emit('letSGo', socket.id);
+  },
 
-var joinRoom = function (socket) {
-   var room = availableRooms.shift() || createRoom();
-   if (socket.rooms[room]) {
-     return room;
-   }
-   socket.join(room);
-   socket.emit('joinRoom', room);
-   games[room].addPlayer(socket);
-   if (games[room].count === 2) {
-     io.to(room).emit('roomFull', games[room]);
-     // setTimeout(function() {
-     play(socket, room);
-     // }, 10000);
-   }
-   return room;
- };
+  createRoom : function (){
+    let room = stagingPoint.uniqueId();
+    availableRooms.push(room);
+    games[room] = newGame(room);
+    return room;
+  },
 
-var cleanRoom = (room) => {
-   if (games[room].count === 0) {
-     let ind = availableRooms.indexOf(room);
-     if (ind >= 0) {
-       availableRooms.splice(ind, 1);
-     }
-     delete games[room];
-   }
-};
-var result = function(data){
-    var score = data;
-    var room = data.room;
-
-    if (score.scorePlayer === score.scorePlayer2) {
-      winner.egal = score.scorePlayer2;
-        
-        io.to(room).emit('result', winner);
+  joinRoom : function (socket) {
+    var room = availableRooms.shift() || stagingPoint.createRoom();
+    if (socket.rooms[room]) {
+      return room;
     }
-    
-    if (score.scorePlayer < score.scorePlayer2) {
-        winner.name = score.namePlayer2;
-        winner.score = score.scorePlayer2;
-
-        io.to(room).emit('result', winner);
-    } 
-    
-    if (score.scorePlayer > score.scorePlayer2) {
-        winner.name = score.idPlayer;
-        winner.score = score.scorePlayer;
-
-        io.to(room).emit('result', winner);
+    socket.join(room);
+    socket.emit('joinRoom', room);
+    games[room].addPlayer(socket);
+    if (games[room].count === 2) {
+      io.to(room).emit('roomFull', games[room]);
+      // setTimeout(function() {
+      stagingPoint.play(socket, room);
+      // }, 10000);
     }
- }
- var register = function(data){
+    return room;
+  },
 
-  console.log('fin')
-  var party = new Party({
-    name: data.name,
-    score: data.score,
-    }).save(function (err, party) {
-    if (err) {
-      console.log(err);
-    } else {
-        console.log('save best player');
-        deleteSocket(data.room)
-    };
-  });
-};
+  clearRoom : function (room) {
+    if (games[room].count === 0) {
+      let ind = availableRooms.indexOf(room);
+      if (ind >= 0) {
+        availableRooms.splice(ind, 1);
+      }
+      delete games[room];
+    }
+  },
+  result : function(data){
+      var score = data;
+      var room = data.room;
 
+      if (score.scorePlayer === score.scorePlayer2) {
+        winner.egal = score.scorePlayer2;
+          
+          io.to(room).emit('result', winner);
+      }
+      
+      if (score.scorePlayer < score.scorePlayer2) {
+          winner.name = score.namePlayer2;
+          winner.score = score.scorePlayer2;
 
-var deleteSocket = function (socket) {
-   delete clients[socket];
- };
+          io.to(room).emit('result', winner);
+      } 
+      
+      if (score.scorePlayer > score.scorePlayer2) {
+          winner.name = score.idPlayer;
+          winner.score = score.scorePlayer;
 
+          io.to(room).emit('result', winner);
+      }
+  },
+  register : function (data){
+
+    console.log('fin')
+    var party = new Party({
+      name: data.name,
+      score: data.score,
+      }).save(function (err, party) {
+      if (err) {
+        console.log(err);
+      } else {
+          console.log('save best player');
+          stagingPoint.deleteSocket(data.room)
+      }
+    });
+  },
+  deleteSocket : function (socket) {
+    delete clients[socket];
+  }
+}
 var connection = function (socket){
 
    socket.on('multiPlayerGame', function(data) {
@@ -132,21 +132,20 @@ var connection = function (socket){
         socket.name = data.user;
         socket.avatar = data.avatar;
         clients[socket.id] = socket;
-        room = joinRoom(socket);
+        room = stagingPoint.joinRoom(socket);
         index = global.players.push(socket.name) - 1;
      
-
-     socket.on('score', function(data) {
-       io.to(room).emit('evolutionScore', data);
-     });
+    socket.on('score', function(data) {
+      io.to(room).emit('evolutionScore', data);
+    });
 
     socket.on('scoreFinal', function(data){
-        result(data)
+      stagingPoint.result(data)
     });
 
      socket.on('finish', function (data) {
         if (save === true) {
-          register(data)
+          stagingPoint.register(data)
           save = false;
           return save;
         };
@@ -156,8 +155,8 @@ var connection = function (socket){
         global.players.splice(index, 1);
         socket.to(room).emit('deconnection', socket.id);
         games[room].removePlayer(socket.id);
-        cleanRoom(room);
-        deleteSocket(socket.id);
+        stagingPoint.clearRoom(room);
+        stagingPoint.deleteSocket(socket.id);
       });
    });
  };
