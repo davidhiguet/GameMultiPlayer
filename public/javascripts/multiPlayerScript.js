1db197cc19b55c7f27ee148c7890ae65deff383c
davidhiguet <38644243+davidhiguet@users.noreply.github.com>
2018-04-28 19:05:04 +0200

    Update multiPlayerScript.js

--- public/javascripts/multiPlayerScript.js
+++ public/javascripts/multiPlayerScript.js
@@ -5,6 +5,13 @@ window.onload = function () {
     var SECOND;
     var room;
     var changement = false;
+    var x = 0;
+    var y = 0;
+    var color = {
+        first: 'yellow',
+        second: 'red'
+    }
+    var size = 100;
 
     /***************************Show the property of the other player in my view****************************/
     var playerDispatch = function () {
@@ -68,7 +75,7 @@ window.onload = function () {
         timer: function () {/*TIMER 15 SECONDE PAR PARTY*/
             document.getElementById('timer').style.display = 'block';
 
-            var timer = 15;
+            var timer = 30;
             var functionInterval1 = setInterval(interVal1, 1000);
 
             function interVal1() {
@@ -99,13 +106,54 @@ window.onload = function () {
             }, 5000);
         }
     }
+    
+    var creationPersonnage = {
+        insertDiv : function (id, color, back) {
+            var x = 0;y = 0;
+            var div = window.document.createElement('div');
+            div.id = id;
+            div.style.left = (x - (size / 2)) + 'px';
+            div.style.top = (y + (size / 2)) + 'px';
+            div.style.border = 10 +'px solid '+ color;
+            div.style.borderRadius = 50+'%';
+            div.style.zIndex = 1000000;
+            div.style.width = size + 'px';
+            div.style.height = size + 'px';
+            div.style.position = 'absolute';
+            div.style.backgroundImage = 'url("/images/'+back+'.png")';
+            div.style.backgroundSize = 'cover';
+            window.document.body.appendChild(div);
+        },
+        updateDivPosition : function (event, id) {
+            
+            var div = window.document.getElementById(id);
+            if (div) {
+                var newLeft = (event.x - 10);
+                var newTop = (event.y + 10);
+                if (newLeft < 0) {
+                    newLeft = 0;
+                }
+                if (newTop < 0) {
+                    newTop = 0;
+                }
+                if ((newLeft + (size)) > document.documentElement.clientWidth) {
+                    newLeft = document.documentElement.clientWidth - (size);
+                }
+                if ((newTop + (size)) > document.documentElement.clientHeight) {
+                    newTop = document.documentElement.clientHeight - (size);
+                }
+                div.style.top = newTop + 'px';
+                div.style.left = newLeft + 'px';
+            }
+        }
+    };
+    
+
     /*************************** function give or take points****************************/
     var clickPlay = function () {
 
         $('.yellow').on('click', function () {
-            //$(".yellow").addClass('red');
-
-
+            
             GAME.players[FIRST].score = GAME.players[FIRST].score + 1;
             var evolutionScore = {
                 idPlayer: GAME.players[FIRST].id,
@@ -113,11 +161,11 @@ window.onload = function () {
             }
 
             socket.emit('score', evolutionScore);
-            //$(".yellow").removeClass('boxOnClick');
+            
         })
         $('.red').on('click', function () {
 
-            $(".red").css('box-shadow', '0', '5px', '42px', 'rgba(0, 0, 0, 0.5)');// = "inset 0 5px 42px rgba(0, 0, 0, 0.5)";
+            $(".red").css('box-shadow', '0', '5px', '42px', 'rgba(0, 0, 0, 0.5)');
             GAME.players[FIRST].score = GAME.players[FIRST].score - 1;
             playerDispatch()
             var evolutionScore = {
@@ -126,7 +174,35 @@ window.onload = function () {
             }
             socket.emit('score', evolutionScore);
         })
+    }
 
+    var touchPlay = function () {
+            var perso1 = document.getElementById(FIRST);
+            perso1X = window.getComputedStyle(perso1, null).getPropertyValue("left");
+            perso1Y = window.getComputedStyle(perso1, null).getPropertyValue("top"); 
+            perso1X = parseFloat(perso1X);
+            perso1Y = parseFloat(perso1Y);
+            console.log('^perso1X '+ perso1X)
+            perso1Width = parseFloat(perso1.style.width);
+            perso1Height = parseFloat(perso1.style.height);
+
+            var perso2 = document.getElementById(SECOND);
+            perso2X = window.getComputedStyle(perso2, null).getPropertyValue("left");
+            perso2Y = window.getComputedStyle(perso2, null).getPropertyValue("top");
+            perso2X = parseFloat(perso2X);
+            perso2Y = parseFloat(perso2Y);
+            perso2Width = parseFloat(perso2.style.width);
+            console.log('^persoX ' + perso2Width)
+            perso2Height = parseFloat(perso2.style.height);
+
+            if (perso1X < perso2X + perso2Width && perso1X + perso1Width > perso2X && perso1Y < perso2Y + perso2Height && perso1Height + perso1Y > perso2Y) {
+
+                document.getElementById(FIRST).style.left = perso1X - 300 +'px';
+                document.getElementById(SECOND).style.left = perso2X + 300 + 'px';
+                console.log('collision')
+                console.log(document.getElementById(FIRST).style.left)
+
+            }        
     }
 
 
@@ -150,7 +226,7 @@ window.onload = function () {
 
 
         socket.on('multiPlayerGame', function (data) {
-            console.log(data)
+            //console.log(data)
             FIRST = data;
             var giveClass = document.getElementsByClassName('first');
             giveClass[0].classList.add(data);
@@ -160,10 +236,23 @@ window.onload = function () {
             console.log('Joined room:', data);
             room = data;
             phase.one();
-            console.log(FIRST)
-
+            console.log('first' + avatarChoosen)
+                /*my perso on the mouse*/
+                creationPersonnage.insertDiv(FIRST, color.first, avatarChoosen);
+                
+                window.document.addEventListener('mousemove', function (event) {
+                    var event = {
+                        x: event.clientX,
+                        y: event.clientY,
+                    }
+                    creationPersonnage.updateDivPosition(event, FIRST);
+                    /*send my position to the other player*/
+                    socket.emit('myPlayer', event)
+                });
+            
+                
         });
-
+        
         /*two player in the room display game area and players property*/
         socket.on('roomFull', function (data) {
             console.log('roomFull', data);
@@ -171,28 +260,37 @@ window.onload = function () {
             for (id in GAME.players) {
                 if (id !== FIRST) {
                     SECOND = id;
+                    console.log('second' + SECOND)
                     var giveClass2 = document.getElementsByClassName('second');
                     giveClass2[0].classList.add(id);
                 }
             }
+            /*creation secondplayer div character*/
+            creationPersonnage.insertDiv(SECOND, color.second, GAME.players[SECOND].avatar );
             playerDispatch();
             phase.two();
 
-
         });
 
-
+        socket.on('playerMove', function (events) {
+            /*receive the position from the other player*/
+            var coords = events;
+            console.log('events'+events)
+            creationPersonnage.updateDivPosition(coords, SECOND);
+        });
 
         /***************************Party started****************************/
-        socket.on('letSGo', function (data) {
+        socket.on('letSGo', function (data, events) {
+            
             clickPlay();
+            touchPlay();
             var startTimer = setTimeout(function () {
                 phase.timer();
             }, 3100);
 
         });
         /***************************Evolution other player score in my view****************************/
-        socket.on('evolutionScore', function (data) {
+        socket.on('evolutionScore', function (data, events) {
             console.log(data)
             $('.' + GAME.players[data.idPlayer].id + ' .property .score').text(data.scorePlayer);
             GAME.players[data.idPlayer].score = data.scorePlayer;
@@ -231,9 +329,6 @@ window.onload = function () {
             phase.end()
 
         });
-
     };
-    connection();
+connection();
 };
-
-
